4f29d8b514d98d312c4cb7d833720e2adf946050
Luke Chen Shui <lukechenshui@Lukes-Macbook-Pro.local>
2025-10-13 14:40:38 -0500

    Modified some db cdk parameters

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,7 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,5 +14,33 @@ export class InfrastructureStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    //
+    const vpc = new Vpc(this, "VPC", {
+      cidr: "10.0.0.0/23",
+      maxAzs: 2,
+    });
+
+    const db = new rds.DatabaseInstance(this, "Database", {
+      engine: rds.DatabaseInstanceEngine.POSTGRES,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 20,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+      credentials: rds.Credentials.fromGeneratedSecret("chenshui_user"),
+      databaseName: "chenshui_clinic_management",
+      backupRetention: cdk.Duration.days(7),
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Might switch this to snapshot later on
+      storageEncrypted: true,
+      storageType: rds.StorageType.GP3,
+      deletionProtection: false,
+      autoMinorVersionUpgrade: true,
+      enablePerformanceInsights: true,
+      performanceInsightRetention: rds.PerformanceInsightRetention.DEFAULT,
+      publiclyAccessible: true, // True only because it needs to be accessible for assessment purposes
+    });
   }
 }
